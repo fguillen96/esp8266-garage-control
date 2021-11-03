945f43407ab388034b74f4a54f4838ddc9e99395
Fran Guillén <franguiare@gmail.com>
2021-11-03 18:56:19 +0100

    Certificate fix (no fingerprint needed)

--- src/main.cpp
+++ src/main.cpp
@@ -6,54 +6,21 @@
 
 #define DEBUGGING
 
-#ifdef DEBUGGING
-  #define DEBUG(x)       Serial.print (x)
-  #define DEBUGLN(x)     Serial.println (x)
-#else
-  #define DEBUG(x)
-  #define DEBUGLN(x) 
-#endif
-
-
+#include <debug.h>
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>
 #include <WiFiClientSecure.h>
-#include <PubSubClient.h>   // MQTT library
-#include <ArduinoJson.h>    // JSON library
+#include <PubSubClient.h>         // MQTT library
+#include <ArduinoJson.h>          // JSON library
 #include <time.h>
 
-
-// ---------- CERTIFICATE ---------
-const char caCert[] PROGMEM = R"EOF(
------BEGIN CERTIFICATE-----
-MIIDtzCCAp+gAwIBAgIUGbIhBoSk1tJJJXA93lIJjmwOWScwDQYJKoZIhvcNAQEL
-BQAwazELMAkGA1UEBhMCRVMxDzANBgNVBAgMBk1hbGFnYTEPMA0GA1UECgwGTE1U
-ZWNoMRcwFQYDVQQDDA5zZWxmY2VydC5sb2NhbDEhMB8GCSqGSIb3DQEJARYSc3Jz
-d29yZWJAZ21haWwuY29tMB4XDTIxMTAxNDIwMTkwNFoXDTMxMTAxMjIwMTkwNFow
-azELMAkGA1UEBhMCRVMxDzANBgNVBAgMBk1hbGFnYTEPMA0GA1UECgwGTE1UZWNo
-MRcwFQYDVQQDDA5zZWxmY2VydC5sb2NhbDEhMB8GCSqGSIb3DQEJARYSc3Jzd29y
-ZWJAZ21haWwuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtDSl
-YvHiCYK3pUTJKHutBvaMTWpMFnWpLWcjVRTdM5mKp7HOaIcsDDud8k6RWFfZZxA8
-UnU6b2HTJw3g2kmw1f9YRDMVf1uHIBoHl4fwjEzEs65AzMughn8SP3e+NyDXSTHo
-60kYoTrZmny+I3rlyJU9QQn2eG0/v7pOzq3qMu/xzWkfMMsPKCzKdBam+RoqgRKE
-o9pXvwXB8oDmQCOfmSYS+FTqR0KcaTfQ6F99HrOcxV2xhMr7w8WWNniG4lSAg78Z
-W0KekZUutUOXZYVjKZGvCIMky08iTgAqGJL9Or2k7jFPMiMp2LMP0SRqA4vdDKrl
-9HR3XoE8NbH5/RZxCwIDAQABo1MwUTAdBgNVHQ4EFgQUniypLIL+OwUxncPzJvFT
-oTyjIcAwHwYDVR0jBBgwFoAUniypLIL+OwUxncPzJvFToTyjIcAwDwYDVR0TAQH/
-BAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAZMFTlWNnAun7YOlCSKJ6MO2C/FqV
-cqHa9COiN7t3OYYuNIE24eEw0UPTgCj9GyTEGJV4BwKtJx2DgegWOKB7gi7XrbQo
-Nna4z1PQ4m0vhPdt17qTLiwmYlU94Uqu2fj8eMNLGX8YNuclJvcNEmBFSMTuQ6Tj
-Vd80Ds0diILs2E8SJjzTqtY/H/1BDogakZtCetcDqcARFnOwqFsWtByxpiHV62dv
-I6skFmTmoI4IRQlywVbZgMYK1bkPxipiAfJjP4SCMILX8QNKQe9Bgel58okp8A/k
-n2gwdZPDUNOzCG6eM3Gnd0rdAwlWfBPi7J0P61uc++cmbDQcUbOCx202ZQ==
------END CERTIFICATE-----
-)EOF";
-
-const char* fingerprint = "2a:9a:ba:d2:76:74:61:e5:39:39:b3:bb:80:2f:b5:07:99:5f:f3:37";
+// ---------- SENSITIVE INFORMATION --------
+#include <secrets.h>
+#include <CertificateFile.h>
 
 
 // ---------- DEFAULT SYSTEM CONFIGURATION ---------
-#define RELAY_TIMEOUT   200
+#define RELAY_TIMEOUT         200
 #define SENSOR_READ_INTERVAL  500
 
 // ---------- DIGITAL INPUTS ----------
@@ -65,21 +32,20 @@ const char* fingerprint = "2a:9a:ba:d2:76:74:61:e5:39:39:b3:bb:80:2f:b5:07:99:5f
 #define DO_PP_RELAY    5
 #define DO_LED         2
 
-// ---------- WIFI AND MQTT CONNECTION ----------
-//#define WIFI_SSID       "Trojan"
-//#define WIFI_PASSWORD   "ShupalaGamba69"
-#define MQTT_SERVER     "185.137.122.40"                 // MQTT server IP
-#define MQTT_PORT       8883                             // MQTT port
-#define MQTT_USERNAME   "liebre"                         // MQTT server username
-#define MQTT_PASSWORD   "magic"                          // MQTT server password
-#define CLIENT_ID       "Piter_garage"                   // MQTT client ID
+// ---------- WIFI AND MQTT CONNECTION (secrets.h) ----------
+#define WIFI_SSID       SECRET_WIFI_SSID               // WIFI SSID
+#define WIFI_PASSWORD   SECRET_WIFI_PASSWORD           // WIFI password
+#define MQTT_SERVER     SECRET_MQTT_SERVER             // MQTT server IP
+#define MQTT_PORT       SECRET_MQTT_PORT               // MQTT port
+#define MQTT_USERNAME   SECRET_MQTT_USERNAME           // MQTT server username
+#define MQTT_PASSWORD   SECRET_MQTT_PASSWORD           // MQTT server password
+#define CLIENT_ID       "Piter_garage"                 // MQTT client ID
 
 // ---------- MQTT TOPICS ----------
 #define TOPIC_PUB_DOOR      "garage/door"
 #define TOPIC_SUB_CONFIG    "garage/config"
 #define TOPIC_SUB_CONTROL   "garage/control"
 #define TOPIC_WILL          "garage/status"
-
 
 
 // ********************************************************************
@@ -105,7 +71,7 @@ unsigned long start_time = 0;
 // ********************************************************************
 void ConnectWifi();
 bool ConnectMQTT();
-unsigned long GetEpochTime();
+unsigned long UnixTime();
 void UpdateDoorStatus(bool);
 void RelayControl(bool);
 
@@ -155,7 +121,6 @@ IRAM_ATTR void SecuritySwitch() {
     digitalWrite(DO_PP_RELAY, HIGH);
     interrupt_flag = true;
   }
-  
 }
 
 
@@ -173,30 +138,32 @@ void setup() {
   pinMode(DO_LED, OUTPUT);
   digitalWrite(DO_LED, HIGH);
   pinMode(DI_SENSOR_DOOR, INPUT_PULLUP);
-  pinMode(DI_SENSOR_SECURITY, INPUT_PULLUP);
+  //pinMode(DI_SENSOR_SECURITY, INPUT_PULLUP);
   pinMode(DO_PP_RELAY, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(DI_SENSOR_SECURITY), SecuritySwitch, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(DI_SENSOR_SECURITY), SecuritySwitch, FALLING);
 
-  // --------- MANAGING CERTS ---------
-  espClient.setTrustAnchors(&caCertX509);
-  espClient.allowSelfSignedCerts();
-  espClient.setFingerprint(fingerprint);
-  //espClient.setInsecure();
 
   // --------- WIFI MANAGER CONFIG --------
   WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP 
   wifiManager.setConfigPortalBlocking(false);
   wifiManager.autoConnect("LMTech");
   //ConnectWifi();
+
+  // --------- NTP SERVER CONFIG ---------
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+    // --------- MANAGING CERTS ---------
+  //espClient.setTrustAnchors(&caCertX509);
+  //espClient.setX509Time(now());
+  //espClient.allowSelfSignedCerts();
+  //espClient.setFingerprint(fingerprint);
+  //espClient.setInsecure();
   
   // ---------- MQTT CONNECTION --------
   // Connection is done in loop (non blocking)
   mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
   mqttClient.setCallback(OnReceiveMQTT);
 
-  // --------- NTP SERVER CONFIG ---------
-  configTime(0, 0, "pool.ntp.org");
-  GetEpochTime();
 }
 
 // ********************************************************************
@@ -236,12 +203,16 @@ void loop() {
 
   // ---------- MANAGE MQTT CONNECTION (NON BLOCKING) ----------
   static unsigned long lastReconnectAttempt = 0;
-  if (!mqttClient.connected()) {
+  if (!mqttClient.connected() && time(nullptr) > 1635448489) {
     if (millis() - lastReconnectAttempt > 5000) {
       lastReconnectAttempt = millis();
       if (ConnectMQTT()) {
+        DEBUGLN(" MQTT connected");
         lastReconnectAttempt = 0;
         UpdateDoorStatus(door_open);
+      }
+      else {
+        DEBUGLN("MQTT connection error");
       }
     }
   }
@@ -275,11 +246,12 @@ void RelayControl(bool activate) {
 
 bool ConnectMQTT() {
   String clientId = CLIENT_ID;
+  // Certificates to connect to server
+  espClient.setTrustAnchors(&caCertX509);
 
   // Attempt to connect
   if (mqttClient.connect(clientId.c_str(), MQTT_USERNAME, MQTT_PASSWORD, TOPIC_WILL, 1, true, "Disconnected")) {
     mqttClient.publish(TOPIC_WILL, "Connected", true);
-    DEBUGLN("Connected to MQTT server");
     mqttClient.subscribe(TOPIC_SUB_CONFIG, 1);
     mqttClient.subscribe(TOPIC_SUB_CONTROL, 0); 
     }
@@ -287,12 +259,6 @@ bool ConnectMQTT() {
   return mqttClient.connected();
 }
 
-// --------- GETTING TIME ---------
-unsigned long GetEpochTime() {
-  time_t current_time =  now();
-  time(&current_time);
-  return current_time;
-}
 
 // --------- UPDATE DOOR STATUS AND SEND MQTT SERVER --------
 void UpdateDoorStatus(bool open) {
@@ -303,7 +269,7 @@ void UpdateDoorStatus(bool open) {
   if (open) doc["status"] = "open";
   else doc["status"] = "closed";
 
-  doc["ts"] = GetEpochTime();
+  doc["ts"] = (int)time(nullptr);
 
   serializeJsonPretty(doc, buffer);
   mqttClient.publish(TOPIC_PUB_DOOR, buffer, true); 
